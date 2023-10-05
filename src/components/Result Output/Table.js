a239493cf1587e3c378deee507b9008b3e3a2f72
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-06 02:04:49 +0530

    Update Table.js

--- src/components/Result Output/Table.js
+++ src/components/Result Output/Table.js
@@ -6,7 +6,7 @@ export default function Table(props) {
   return (
     <table className={style.result}>
       <TableHeadings />
-      <TableData calculation={props.result} />
+      <TableData calculation={props.data} contribution={props.investment}/>
     </table>
   );
 }
